b10ea13bf3b1fb5b8276636a2f3a927702715d91
ulsup <supruniuk@ucu.edu.ua>
2017-02-09 10:58:34 +0200

    Add files via upload

--- battleship.py
+++ battleship.py
@@ -1,11 +1,11 @@
 def read_field(filename):
     '''
     (str) -> (data)
-    Read element from file and write it down to any data format.
+    Reads element from file and writes it down to the data format.
     '''
     f = open(filename, 'r', encoding='utf-8')
     sequence = f.readlines()
-    sequence = [w.strip('\n') for w in sequence]
+    sequence = [w.strip('\n') for w in sequence]  # creates a list with strings in it (1 string = 1 line)
     f.close()
     return sequence
 print(read_field('field.txt'))
@@ -18,9 +18,9 @@ def has_ship(sequence, point):
     import string
     letters = [i for i in string.ascii_uppercase[:10]]
     numbers = [i for i in range(1,11)]
-    tuples = [(a,b) for a in letters for b in numbers]
-    pairs = [(c,letters.index(c)+1) for c in letters]
-    for i in pairs:
+    tuples = [(a,b) for a in letters for b in numbers]  # creates all possible coordinates in tuples
+    pairs = [(c,letters.index(c)+1) for c in letters]  # to create tuple of letters and its indexes
+    for i in pairs:  # its easier to switch letter to number and number to letter
         if point[0] in i:
             poryadok = i[1] - 1
     if point in tuples:
@@ -46,94 +46,93 @@ def ship_size(sequence, point):
         if point[0] in i:
             poryadok = i[1] - 1
             break
-    coordinates = point[1] - 1
-    ryadok = sequence[coordinates]
+    line = point[1] - 1
+    ryadok = sequence[line]
     if point in tuples:
-        if ryadok[poryadok] == '*':
+        if ryadok[poryadok] == '*':  # checks if on this area placed ship or its part
             count = 1
             right = 0
             left = 0
             up = 0
             down = 0
             lst = []
-            lst.append((point[0], coordinates + 1))
-            try:
-                while ryadok[poryadok + 1] == '*':
-                    count += 1
-                    poryadok += 1
-                    right += 1
-                    if right != 0:
-                        for i in pairs:
-                            if (poryadok + 1) in i:
-                                new_point = i[0]
-                                break
-                        lst.append((new_point, coordinates + 1))
-            except IndexError:
-                pass
-            poryadok -= right
-            try:
-                while ryadok[poryadok - 1] == '*':
-                    if poryadok == 0:
-                        break
-                    count += 1
-                    poryadok -= 1
-                    left -= 1
-                    if left != 0:
-                        for i in pairs:
-                            if (poryadok + 1) in i:
-                                new_point = i[0]
-                        lst.append((new_point, coordinates + 1))
-            except IndexError:
-                pass
-            poryadok += left
-            try:
-                while sequence[coordinates + 1][poryadok] == '*':
-                    count += 1
-                    coordinates += 1
-                    down += 1
-                    if down != 0:
-                        for i in pairs:
-                            if (poryadok + 1) in i:
-                                new_point = i[0]
-                                break
-                        lst.append((new_point, coordinates + 1))
-            except IndexError:
-                pass
-            coordinates -= down
-            try:
-                while sequence[coordinates - 1][poryadok] == '*':
-                    count += 1
-                    coordinates -= 1
-                    up += 1
-                    if up != 0:
-                        for i in pairs:
-                            if (poryadok + 1) in i:
-                                new_point = i[0]
-                                break
-                        lst.append((new_point, coordinates - 1))
-            except IndexError:
-                pass
-            coordinates += up
+            lst.append((point[0], line + 1))  # addes coordinates of current point
+            if ryadok[poryadok + 1] == '*' or ryadok[poryadok - 1] == '*':  # checks if ship has more parts on the right
+                try:  # or on the left sides. if it hasn't, it checks whether ship has parts below or under its part
+                    while ryadok[poryadok + 1] == '*':  # switches positions within one line
+                        count += 1
+                        poryadok += 1
+                        right += 1  # this variable is to count how many positions we moved on
+                        if right != 0:
+                            for i in pairs:
+                                if (poryadok + 1) in i:
+                                    new_point = i[0]
+                                    break
+                            lst.append((new_point, line + 1))  # addes coordinate of '*' until not ''
+                except IndexError:
+                    pass
+                poryadok -= right  # to get back previous value of variable poryadok
+                try:
+                    while ryadok[poryadok - 1] == '*':
+                        if poryadok == 0:  # if index of '*' is 0, we don't need to go to the last element (index -1)
+                            break  # so it finishes counting the length
+                        count += 1
+                        poryadok -= 1
+                        left -= 1
+                        if left != 0:
+                            for i in pairs:  # switches number to the letter
+                                if (poryadok + 1) in i:
+                                    new_point = i[0]
+                            lst.append((new_point, line + 1))
+                except IndexError:
+                    pass
+                poryadok += left
+            elif sequence[line + 1][poryadok] == '*' or sequence[line - 1][poryadok] == '*':  # checks whether ship has
+                try:  # parts below or under its part
+                    while sequence[line + 1][poryadok] == '*':  # switches lines but moves on the same position
+                        count += 1
+                        line += 1
+                        down += 1
+                        if down != 0:
+                            for i in pairs:
+                                if (poryadok + 1) in i:
+                                    new_point = i[0]
+                                    break
+                            lst.append((new_point, line + 1))
+                except IndexError:
+                    pass
+                line -= down
+                try:
+                    while sequence[line - 1][poryadok] == '*':
+                        count += 1
+                        line -= 1
+                        up += 1
+                        if up != 0:
+                            for i in pairs:
+                                if (poryadok + 1) in i:
+                                    new_point = i[0]
+                                    break
+                            lst.append((new_point, line - 1))
+                except IndexError:
+                    pass
+                line += up
         else:
              return 'There is no ship in this area.'
         return (count, lst)
     else:
         return 'You should enter uppercase letters between A and J and numbers between 1 and 10'
 
-print(ship_size(read_field('field.txt'), ('I', 6)))
+print(ship_size(read_field('field.txt'), ('B', 7)))
 
 def is_valid(sequence):
     '''
     (data) -> (bool)
     Checks if the field is correct including sizes of ships and intersection, size of field.
     '''
-    import collections
-    new_seq = sum([i.split(' ') for i in sequence], [])
-    counter = collections.Counter(new_seq)
-    if counter['*'] == 4 and counter['**'] == 3 and counter['***'] == 2 and counter['****'] == 1:
-        return True
-    else:
-        return False
+    for line in sequence:
+        for element in line:
+            if element == '*':
+                pass
 
 print(is_valid(read_field('field.txt')))
 
